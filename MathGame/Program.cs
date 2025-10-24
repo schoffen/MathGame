d284213556ad3c42aaf86c55d963711baf59614c
Felipe Schoffen <fs.tressino@gmail.com>
2025-10-24 05:32:11 -0300

    refactor: change games to a single method feat: implemented all operations

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,6 +1,8 @@
 ﻿const int minOption = 1;
 const int maxOption = 5;
 
+// TODO store previous games
+
 do
 {
     ShowInitialMenu();
@@ -22,10 +24,11 @@ void ShowInitialMenu()
     Console.WriteLine("Welcome to the Math Game!");
     Console.WriteLine("Select a operation to start or press any key to exit.");
     Console.WriteLine("1 - Addition");
-    Console.WriteLine("2 - Subtration");
+    Console.WriteLine("2 - Subtraction");
     Console.WriteLine("3 - Multiplication");
     Console.WriteLine("4 - Division");
     Console.WriteLine("5 - Show previous records");
+    Console.Write("Option: ");
 }
 
 (bool isValid, int option) IsInputValid(string? option)
@@ -40,51 +43,80 @@ int GenerateNumber()
     return (int)random.NextInt64(0, 100);
 }
 
-void StartGame(int option)
-{   Console.Clear();
-    int correctAnswers = 0;
-    
-    switch (option)
-    {
-        case 1:
-            bool failed = false;
-            do
-            {
-                if (Addition())
-                {
-                    correctAnswers++;
-                    Console.WriteLine("That's correct! Press any key to continue");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine($"Incorrect. Your score was {correctAnswers} correct answers.\nPress any key to return to the menu");
-                    Console.ReadKey();
-                    failed = true;
-                }
-            } while (!failed);
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-    }
+void DisplayCorrectAnswer()
+{
+    Console.WriteLine("That's correct! Press any key to continue");
+    Console.ReadKey();
 }
 
-bool Addition()
+void DisplayIncorrectAnswer(int streak)
+{
+    Console.WriteLine($"Incorrect. Your score was {streak} correct answers.\nPress any key to return to the menu");
+    Console.ReadKey();
+}
+
+void StartGame(int option)
+{   
+    Console.Clear();
+    int streak = 0;
+    
+    bool failed = false;
+    do
+    {
+        if (Question(option))
+        {
+            streak++;
+            DisplayCorrectAnswer();
+        }
+        else
+        {
+            DisplayIncorrectAnswer(streak);
+            failed = true;
+        }
+    } while (!failed);
+    
+    Console.Clear();
+}
+
+bool Question(int operation)
 {
     Console.Clear();
     (int a, int b) = (GenerateNumber(), GenerateNumber());
-    int answer = a + b;
+    int answer = 0;
 
+    switch (operation)
+    {
+        case 1:
+            answer = a + b;
+            Console.WriteLine($"{a} + {b} = ?");
+            break;
+        case 2:
+            answer = a - b;
+            Console.WriteLine($"{a} - {b} = ?");
+            break;
+        case 3:
+            answer = a * b;
+            Console.WriteLine($"{a} * {b} = ?");
+            break;
+        case 4:
+            while (b == 0 || a % b != 0)
+            {
+                a = GenerateNumber();
+                b = GenerateNumber();
+            }
+
+            answer = a / b;
+            Console.WriteLine($"{a} / {b} = ?");
+            break;
+        case 5:
+            
+            break;
+        default:
+            throw new ArgumentOutOfRangeException(nameof(operation), operation, null);
+    }
+    
     var input = "";
     int guess;
-    
-    Console.WriteLine($"{a} + {b} = ?");
     
     do
     {
